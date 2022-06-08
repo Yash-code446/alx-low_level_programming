d7028c401159caf74c5120d2d5fee2297bb1378b
Yash-code446 <yashna.maikoo@gmail.com>
2022-06-08 11:21:09 -0700

    hot fix 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Prints "_putchar" followed by a new line
- * Return: Always 0 (success)
- */
+* main - Prints "_putchar" followed by a new line
+* Return: Always 0 (success)
+*/
 int main (void)
 {
 	_putchar('_');
